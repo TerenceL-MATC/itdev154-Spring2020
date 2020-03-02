dcc61590563616bf5e0aae86273619c174447b4a
Terence Lee <leet23@gmatc.matc.edu>
2020-03-02 09:39:09 -0600

    Project fully functional

---
+++ MyLinkedListProject/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLinkedListProject
+{
+    class Node
+    {
+        public int data;
+        public Node link;
+
+        public Node(int info)
+        {
+            data = info;
+            link = null;
+        }
+    }
+}
