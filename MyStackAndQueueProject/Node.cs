286672606c68924dd8f3f79618ef4cbc0a023b81
Terence Lee <leet23@gmatc.matc.edu>
2020-03-14 20:46:32 -0500

    Assignment 4 fully functional

---
+++ MyStackAndQueueProject/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStackAndQueueProject
+{
+    class Node
+    {
+        public int data;
+        public Node link;
+
+        public Node(int info)
+        {
+            data = info;
+            link = null;
+        }
+    }
+}
