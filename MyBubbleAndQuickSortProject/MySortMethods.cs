4002ba3a74292c8fd330f8a117e1a4bf94b79a85
Terence Lee <leet23@gmatc.matc.edu>
2020-05-02 16:16:10 -0500

    Assignement 7 fully operational

---
+++ MyBubbleAndQuickSortProject/MySortMethods.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace MyBubbleAndQuickSortProject
+{
+    static class MySortMethods
+    {
+        public static void BubbleSort(this int[] numberList)
+        {
+            if(numberList.Length is 0)
+            {
+                WriteLine(@"Can't sort an empty list.");
+                return;
+            }
+
+            int swapsMade,    //How many position swaps occured during current pass
+                firstNumber,
+                secondNumber,
+                temp;
+            
+            //Increments the number of passes made through the list
+            for(int onPassNumber = 1; onPassNumber < numberList.Length; onPassNumber++)
+            {
+                swapsMade = 0;
+                firstNumber = 0;
+                secondNumber = 1;
+
+                while(firstNumber < numberList.Length - onPassNumber) 
+                {
+                    if(numberList[firstNumber] > numberList[secondNumber])
+                    {
+                        temp = numberList[firstNumber];
+                        numberList[firstNumber] = numberList[secondNumber];
+                        numberList[secondNumber] = temp;
+                        swapsMade++;
+                    }
+
+                    firstNumber++;
+                    secondNumber++;
+                }
+
+                if(swapsMade is 0) //List is completely sorted
+                {
+                    break;
+                }
+
+                //Details on the completed pass
+                Write($"After Pass {onPassNumber}: ");
+                numberList.PrintList();
+                WriteLine();
+            }
+
+            WriteLine("List is completely sorted.");
+        }
+
+        public static void QuickSort(this int[] numberList)
+        {
+            numberList.QuickSort(0, numberList.Length - 1); 
+        }
+
+        private static void QuickSort(this int[] numberList, int startIndex, int endIndex)
+        {
+            if(endIndex <= startIndex) //If there's less than 2 elements in an array
+            {
+                return;
+            }
+
+            int pivot = numberList[startIndex],
+                low = startIndex,
+                high = endIndex + 1,
+                temp;
+            
+            //Prints the list passed to the method
+            Write("The list is: ");
+            numberList.PrintList(startIndex, endIndex);
+
+            //Shift elements to their proper side and finds where pivot should be placed.
+            while(high >= low)
+            {
+                low++;
+                high--;
+
+                while(low <= endIndex)
+                {
+                    if(numberList[low] > pivot)
+                    {
+                        break;
+                    }
+
+                    low++;
+                }
+
+                while(high >= low)
+                {
+                    if(numberList[high] < pivot)
+                    {
+                        break;
+                    }
+
+                    high--;
+                }
+
+                if(high < low) //Identified the likely location to move pivot to
+                {
+                    if (numberList[high] < pivot)
+                    {
+                        numberList[startIndex] = numberList[high];
+                        numberList[high] = pivot;
+                    }
+                }
+                else //Identified two numbers that need to be swapped
+                {
+                    temp = numberList[low];
+                    numberList[low] = numberList[high];
+                    numberList[high] = temp;
+                }
+            }
+            
+            //More details about the list
+            WriteLine($"{pivot} is the pivot.");
+            Write("After shifting around the pivot, the list looks like: ");
+            numberList.PrintList(startIndex, endIndex);
+            Write("Left Sublist: ");
+            numberList.PrintList(startIndex, high - 1);
+            Write("Right Sublist: ");
+            numberList.PrintList(low, endIndex);
+            WriteLine();
+
+            //Recursive calls to quick sort the list
+            numberList.QuickSort(startIndex, high - 1); //Quick sorts the left sublist
+            numberList.QuickSort(low, endIndex);  //Quick sorts the right sublist
+        }
+
+        public static void PrintList(this int[] numberList)
+        {
+            for (int i = 0; i < numberList.Length; i++)
+            {
+                Write(numberList[i]);
+                if (i != numberList.Length - 1)
+                {
+                    Write(", ");
+                }
+            }
+
+            WriteLine();
+        }
+
+        public static void PrintList(this int[] numberList, int startIndex, int endIndex)
+        {
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Write(numberList[i]);
+                if (i != endIndex)
+                {
+                    Write(", ");
+                }
+            }
+
+            WriteLine();
+        }
+    }
+}
---
+++ MyBubbleAndQuickSortProject/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MyBubbleAndQuickSortProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decision;
+            int[] createdList;
+
+            while(true)
+            {
+                decision = MainMenu();
+
+                if(decision is 3)
+                {
+                    break;
+                }
+
+                createdList = CreateList();
+                AppOperations(createdList, decision);
+
+                WriteLine("Press any key to clear the screen");
+                ReadKey();
+                Clear();
+            }
+        }
+
+        static int MainMenu()
+        {
+            int choice;
+
+            WriteLine();
+            WriteLine("This app will let you see the inner workings of a bubble sort and a quick sort.");
+            WriteLine();
+            WriteLine("Please select an option!");
+            WriteLine("1. Bubble Sort");
+            WriteLine("2. Quick Sort");
+            WriteLine("3. Quit");
+            Write("Your choice: ");
+
+            while(!int.TryParse(ReadLine(), out choice) || choice < 1 || choice > 3)
+            {
+                WriteLine(@"Sorry, that option doesn't exist!  Please try again.");
+                Write("Your choice (1-3): ");
+            }
+
+            return choice;
+        }
+
+        static int[] CreateList()
+        {
+            int enteredNumber;
+            string enteredValue;
+            List<int> newList = new List<int>();
+
+            WriteLine("\nEnter a list of numbers below, one at a time");
+            WriteLine("Hit the Enter key without typing anything signal that you done entering the list.");
+
+            for(int i = 1; ; i++)
+            {
+                Write($"{i}: ");
+                enteredValue = ReadLine();
+
+                while(!int.TryParse(enteredValue, out enteredNumber) && enteredValue != "")
+                {
+                    WriteLine("Only whole numbers can be accepted!  Please try again.");
+                    Write($"{i}: ");
+                    enteredValue = ReadLine();
+                }
+
+                if(enteredValue is "")
+                {
+                    WriteLine("List has been finalized.");
+                    Write(@"Let's see how this sort works!");
+                    Thread.Sleep(1000);
+                    Write('.');
+                    Thread.Sleep(1000);
+                    Write('.');
+                    Thread.Sleep(1000);
+                    Write('.');
+                    Thread.Sleep(1000);
+                    Clear();
+                    return newList.ToArray();
+                }
+                else
+                {
+                    newList.Add(enteredNumber);
+                }
+            }
+        }
+
+        static void AppOperations(int[] userList, int choice)
+        {
+            WriteLine();
+
+            switch (choice)
+            {
+                case 1: //Bubble Sort
+                    Write("Your list started as: ");
+                    userList.PrintList();
+                    WriteLine();
+                    userList.BubbleSort();
+                    break;
+                case 2: //Quick Sort
+                    userList.QuickSort();
+                    Write($"The sorted list looks like: ");
+                    userList.PrintList();
+                    break;
+                default:
+                    WriteLine(@"You somehow picked an option that doesn't exist.  There isn't any to do here.");
+                    break;
+            }
+
+            WriteLine();
+        }
+    }
+}
